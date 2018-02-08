d1fd9feaa8030d3927562ee5b39e7ba523a35fcc
Tao <wanting.tao@gmail.com>
2018-02-08 16:17:40 -0500

    Add files via upload
    add sample for plotting them in groups

--- 3dplot.py
+++ 3dplot.py
@@ -15,16 +15,21 @@ data = np.loadtxt(r"C:\Users\wtao\Desktop\AC_CON2.csv", delimiter=',', skiprows=
 # the one works
 data = pd.read_csv(r"C:\Users\wtao\Desktop\neilson_different_channels.csv")
 
+#take a subset of data only contain obs have channel = all channel
+data[data.CHANNEL=='ALL CHANNEL']
+
 print(data)
+
+d=pd.Series.tolist(data[data.CHANNEL=='ALL CHANNEL'].PLANNED)
 
 
 x = pd.Series.tolist(data.PLANNED)
 y = pd.Series.tolist(data.AUTOPILOT)
 z = pd.Series.tolist(data.Price_Consider)
 
-print(x,y,z)
+print(d)
 
-# actual plotting
+# actual plotting which doesn't work now
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -37,16 +42,52 @@ Axes3D.scatter(x, y, z, zdir='z', s=20, c=None,depthshade=True)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+#transfer the variable to list
 x = pd.Series.tolist(data.PLANNED)
 y = pd.Series.tolist(data.AUTOPILOT)
 z = pd.Series.tolist(data.Price_Consider)
 
+# add filter for dataset
+d=pd.Series.tolist(data[data.CHANNEL=='GROCERY STORE(Conventional)'].PLANNED)
+e=pd.Series.tolist(data[data.CHANNEL=='GROCERY STORE(Conventional)'].AUTOPILOT)
+f=pd.Series.tolist(data[data.CHANNEL=='GROCERY STORE(Conventional)'].Price_Consider)
 
-ax.scatter(x, y, z, c='r', marker='o')
+#main body for 3d plotting
+ax.scatter(d, e, f, c='r', marker='o')
 
 ax.set_xlabel('PLANNED')
 ax.set_ylabel('AUTOPILOT')
 ax.set_zlabel('Price_Consider')
 
+plt.show()
+
+
+
+### second steps, trying to ploting by groups
+### import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+ 
+# Create data
+N = 60
+g1 = (0.6 + 0.6 * np.random.rand(N), np.random.rand(N),0.4+0.1*np.random.rand(N))
+g2 = (0.4+0.3 * np.random.rand(N), 0.5*np.random.rand(N),0.1*np.random.rand(N))
+g3 = (0.3*np.random.rand(N),0.3*np.random.rand(N),0.3*np.random.rand(N))
+ 
+data = (g1, g2, g3)
+colors = ("red", "green", "blue")
+groups = ("coffee", "tea", "water") 
+ 
+# Create plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
+ax = fig.gca(projection='3d')
+ 
+for data, color, group in zip(data, colors, groups):
+    x, y, z = data
+    ax.scatter(x, y, z, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
+ 
+plt.title('Matplot 3d scatter plot')
+plt.legend(loc=2)
 plt.show()
 
